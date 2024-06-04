716bc4dbdd9b633907942c3198060968c4a6bc91
Christopher David <chris@arcadelabs.co>
2024-06-04 16:40:43 -0700

    clean up restore history

--- igeagent.js
+++ igeagent.js
@@ -5,21 +5,52 @@ const { NostrEvent } = require("@nostr-dev-kit/ndk");
  *  using a shared archive & registry of actions via the Nostr protocol.
  */
 class IGEAgent {
-  /**
-   * Begin exploration process for a given prompt.
-   * @param {string} NostrEvent - The event
-   */
-  parseEvent(event) {
-    console.log("Parsing event:", event);
+  constructor() {
+    this.history = []; // Later rebuild this from local file or Nostr events
   }
 
   /**
    * Begin exploration process for a given prompt.
-   * @param {string} NostrEvent - The prompt from user.
+   * @param {NostrEvent} event - The event
+   */
+  parseEvent(event) {
+    console.log("Parsing event kind " + event.kind + ", id: " + event.id);
+    // If the event is kind 38000, parse a Magency prompt
+    if (event.kind === 38000) {
+      this.explore(event);
+    } else {
+      console.log("Unhandled event kind", event.kind);
+    }
+  }
+
+  /**
+   * Retrieve or create history for a given event.
+   * @param {NostrEvent} event - The event
+   * @returns {Object} The event history.
+   */
+  getHistory(event) {
+    // Look up history by event.id
+    let thisEventHistory;
+    if (this.history.find((element) => element.id === event.id)) {
+      // If so, return the response from this.history[]
+      thisEventHistory = this.history.find((element) => element.id === event.id);
+    } else {
+      // If not, create a new entry in this.history[] with event.id
+      thisEventHistory = { id: event.id, actions: [] };
+      this.history.push(thisEventHistory);
+    }
+    return thisEventHistory;
+  }
+
+  /**
+   * Begin exploration process for a given prompt.
+   * @param {NostrEvent} event - The event
    * @returns {Object} The response data.
    */
-      // Look to see if the spell is in the archive; look up by event id?
-    // For now assume it's new and add it to the archive
+  explore(event) {
+    const thisEventHistory = this.getHistory(event);
+    console.log("This event history:", thisEventHistory);
+  }
 }
 
 module.exports = { IGEAgent };
